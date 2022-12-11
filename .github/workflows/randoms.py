76e9fd105c0e7fb3fd8511dfc0b5af8957b9f0bd
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-11 16:54:39 -0500

    Update randoms.py

--- .github/workflows/randoms.py
+++ .github/workflows/randoms.py
@@ -10,11 +10,22 @@ def make_index(folder):
     """
     index = open(os.path.join(folder, 'index.html'), 'w')
     index.write('<html><body>\n')
-    for root, dirs, files in os.walk(folder):
-        for file in files:
-            if file != 'index.html':
-                index.write('<a href="{}">{}</a><br>\n'.format(os.path.join(root, file), file))
+    
+    for f in os.listdir(path):
+        if os.path.isdir(os.path.join(path, f)):
+            index.write(<li><a href='{}/index.html'>{}</a></li>".format(f, f))
+        else:
+            if file != 'index.html': 
+               index.write("<li><a href='{}'>{}</a></li>".format(f, f))
+            
+   # for root, dirs, files in os.walk(folder):
+       # for file in files:
+            #if file != 'index.html':
+              #  if os.path.isdir(os.path.join(root, file)):
+             #       index.write('<a href="{}">{}</a><br>\n'.format(os.path.join(root, file), file))
     index.write('</body></html>\n')
     index.close()
 
 make_index('./')
+
+
