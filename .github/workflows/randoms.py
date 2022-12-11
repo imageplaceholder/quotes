d5562cf0aef81a4727a153a40b521480376b5acb
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-11 13:36:33 -0500

    Update randoms.py

--- .github/workflows/randoms.py
+++ .github/workflows/randoms.py
@@ -17,7 +17,7 @@ def get_json_files(user):
     for repo in repos:
         repo_name = repo["name"]
         repo_url = repo["url"]
-        repo_response = requests.get("https://api.github.com/repos/{}/contents".format(user))
+        repo_response = requests.get("https://api.github.com/repos/{}/{}/contents/".format(user, str(repo_name)))
         repo_files = repo_response.json()
         for repo_file in repo_files:
             print(repo_files)
