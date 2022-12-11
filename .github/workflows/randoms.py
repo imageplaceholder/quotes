1eeb650d8a524d18ba64d7161729b797004d3e01
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-11 16:05:15 -0500

    Update randoms.py

--- .github/workflows/randoms.py
+++ .github/workflows/randoms.py
@@ -1,64 +1,20 @@
 """
-1. Create a function to look for all json files in a GitHub user repos. And add JSON data & repo name to one single JSON file.
+1. Create a function to make a HTML index of all files in a folder & sub folder.
 """
 
 import os
-import json
-import requests
 
-def get_json_files(user):
+def make_index(folder):
     """
-    Get all json files from a GitHub user repos.
+    Create a HTML index of all files in a folder & sub folder.
     """
-    url = "https://api.github.com/users/{}/repos".format(user)
-    response = requests.get(url)
-    repos = response.json()
-    json_files = []
-    repo_names = []
-    for repo in repos:
-        repo_name = repo["name"]
-        repo_url = repo["url"]
-        repo_response = requests.get("https://api.github.com/repos/{}/{}/contents/".format(user, repo_name))
-        repo_files = json.loads(json.dumps(repo_response.json()))
-   #     repo_files = json.dumps(valid_response)
+    index = open(os.path.join(folder, 'index.html'), 'w')
+    index.write('<html><body>\n')
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file != 'index.html':
+                index.write('<a href="{}">{}</a><br>\n'.format(os.path.join(root, file), file))
+    index.write('</body></html>\n')
+    index.close()
 
-
-        for repo_file in repo_files:
-            print(repo_file)
-            if repo_file["name"].endswith(".json"):
-                json_files.append(repo_file["download_url"])
-               # json_files.append(repo_name)
-    return json_files #, repo_name
-
-#def get_json_data(json_files, repo_name):
-def get_json_data(json_files):
-    """
-    Get JSON data from a list of JSON files.
-    """
-    json_data = []
-    for json_file in json_files:
-        response = requests.get(json_file)
-        json_data.append(json.loads(json.dumps(response.json())))
-       # json_data[-1]['repo_name'] = repo_name                 
-       # json_data['repo_name'] = repo_name[json_file]                 
-        
-    return json_data
-
-def create_json_file(json_data, user):
-    """
-    Create a JSON file with JSON data & repo name.
-    """
-    json_file = {}
-    json_file["data"] = json_data
-    with open("{}.json".format(user), "w") as f:
-        json.dump(json_file, f, indent=4)
-
-def main():
-    user = "imageplaceholder"
-    #json_files, repo_name = get_json_files(user)
-    json_files = get_json_files(user)    
-    json_data = get_json_data(json_files)
-    create_json_file(json_data, user)
-
-if __name__ == "__main__":
-    main()
+make_index('./')
