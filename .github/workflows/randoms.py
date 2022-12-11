5e79bf654a9c11edef9951984cc7c38bc8d7794c
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-11 15:17:38 -0500

    Update randoms.py

--- .github/workflows/randoms.py
+++ .github/workflows/randoms.py
@@ -36,7 +36,7 @@ def get_json_data(json_files, repo_name):
     for json_file in json_files:
         response = requests.get(json_file)
         json_data.append(json.loads(json.dumps(response.json())))
-        json_data[-1]['repo_name'] = repo_name                 
+      ##  json_data[-1]['repo_name'] = repo_name                 
         
     return json_data
 
