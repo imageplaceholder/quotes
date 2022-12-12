26bbd775e0fdb1e292702d969e4060bf46546ada
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-11 21:56:47 -0500

    Revert wip

--- .github/workflows/randoms.py
+++ .github/workflows/randoms.py
@@ -1,55 +1,69 @@
-"""
-1. Create a class to generate a HTML index of a website, with option to exclude file names and folders.  
-"""
+## Project: Create a function to make a HTML index of all files in a folder & sub-folders (optional). So it can be used to directory list all files on a static
+## website like GitHub Pages. 
+
+### To do 
+# get modified dates
+# get all files in sub / sub folders. 
+# icons for file types 
+# add go back to parent directory in HTML for sub folders.
+# add breadcrumbs to show where in directory you are. 
+# add way to exclude files from directory listing.
+# add way to exclude folders from directory listing. 
+# improve / fix proper usage of checking if folder.
+# avoid all .github related folder (.git / .github etc..)
+# use jinja template for easier customization. 
+# make function to make a single index in a single folder. (ie; do not add index.html to folder files.)
+
+### Javascript To Do
+## Click to sort. 
+## Possibily a search feature bar.
+
+
+
 
 import os
-import sys
-import argparse
 
-class IndexGenerator:
-    def __init__(self, path, exclude_files, exclude_folders):
-        self.path = path
-        self.exclude_files = exclude_files
-        self.exclude_folders = exclude_folders
-        self.index_file = os.path.join(self.path, 'index.html')
-        self.index_file_content = ''
-        self.index_file_content += '<html>\n'
-        self.index_file_content += '<head>\n'
-        self.index_file_content += '<title>Index of ' + self.path + '</title>\n'
-        self.index_file_content += '</head>\n'
-        self.index_file_content += '<body>\n'
-        self.index_file_content += '<h1>Index of ' + self.path + '</h1>\n'
-        self.index_file_content += '<table>\n'
-        self.index_file_content += '<tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>\n'
-        self.index_file_content += '<tr><th colspan="5"><hr></th></tr>\n'
-        self.index_file_content += '<tr><td valign="top"><img src="/icons/back.gif" alt="[PARENTDIR]"></td><td><a href="/">Parent Directory</a></td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>\n'
-        self.index_file_content += '<tr><th colspan="5"><hr></th></tr>\n'
+def make_index(folder):
+    """
+    Create a HTML index of all files in a folder.
+    """
+    
+   # if os.path.dirname(folder) in ["git", "github"]:
+    #    return
+        
+    index = open(os.path.join(folder, 'index.html'), 'w')
+    index.write('<html><body>\n')
+    
+    Directories = ""
+    Files = ""
+    print(os.path.dirname(folder))
+    for f in os.listdir(folder):
+        if os.path.isdir(os.path.join(folder, f)):
+            if f not in [".git", ".github"]:
+                Directories += "<li><a href='{}/index.html'>{}</a></li>".format(f, f)
+        else:
+            if f != 'index.html': 
+               Files += "<li><a href='{}'>{}</a></li>".format(f, f)
+               #index.write("<li><a href='{}'>{}</a></li>".format(f, f))
+            
+    index.write(Directories + Files + '</body></html>\n')
+    index.close()
 
-    def generate_index(self):
-        for root, dirs, files in os.walk(self.path):
-            for file in files:
-                if file not in self.exclude_files:
-                    file_path = os.path.join(root, file)
-                    file_size = os.path.getsize(file_path)
-                    file_last_modified = os.path.getmtime(file_path)
-                    self.index_file_content += '<tr><td valign="top"><img src="/icons/text.gif" alt="[TXT]"></td><td><a href="' + file + '">' + file + '</a></td><td align="right">' + str(file_last_modified) + '</td><td align="right">' + str(file_size) + '</td><td>&nbsp;</td></tr>\n'
-            for dir in dirs:
-                if dir not in self.exclude_folders:
-                    dir_path = os.path.join(root, dir)
-                    dir_size = os.path.getsize(dir_path)
-                    dir_last_modified = os.path.getmtime(dir_path)
-                    self.index_file_content += '<tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="' + dir + '">' + dir + '</a></td><td align="right">' + str(dir_last_modified) + '</td><td align="right">' + str(dir_size) + '</td><td>&nbsp;</td></tr>\n'
-        self.index_file_content += '</table>\n'
-        self.index_file_content += '</body>\n'
-        self.index_file_content += '</html>\n'
-        with open(self.index_file, 'w') as f:
-            f.write(self.index_file_content)
 
-if __name__ == '__main__':
-    #parser = argparse.ArgumentParser(description='Generate a HTML index of a website, with option to exclude file names and folders.')
-    #parser.add_argument('-p', '--path', help='Path to the website', required=True)
-    #parser.add_argument('-ef', '--exclude_files', help='File names to exclude', nargs='+')
-   # parser.add_argument('-efd', '--exclude_folders', help='Folder names to exclude', nargs='+')
-  #  args = parser.parse_args()
-    index_generator = IndexGenerator("./", "", [".github", "github"])
-    index_generator.generate_index()
+
+
+
+def create_index_for_sub_folder(path):
+    """
+    Function to create index file for every sub folder inside of a path.
+    """
+    make_index(path)
+    for f in os.listdir(path):
+        print(path)
+        if os.path.isdir(os.path.join(path, f)):
+            if f not in ["git", "github", ".github"]:
+                make_index(os.path.join(path, f))
+
+                
+## pass the path to make HTML index directorys for                
+create_index_for_sub_folder("./")
