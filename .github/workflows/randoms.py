f7c9e4855682291a071c821b7f988612fed2eb65
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-11 19:35:46 -0500

    Update randoms.py

--- .github/workflows/randoms.py
+++ .github/workflows/randoms.py
@@ -11,19 +11,22 @@ def make_index(folder):
     index = open(os.path.join(folder, 'index.html'), 'w')
     index.write('<html><body>\n')
     
+    Directories = ""
+    Files = ""
     for f in os.listdir(folder):
         if os.path.isdir(os.path.join(folder, f)):
-            index.write("<li><a href='{}/index.html'>{}</a></li>".format(f, f))
+            Directories += "<li><a href='{}/index.html'>{}</a></li>".format(f, f)
         else:
             if f != 'index.html': 
-               index.write("<li><a href='{}'>{}</a></li>".format(f, f))
+               Files += "<li><a href='{}'>{}</a></li>".format(f, f)
+               #index.write("<li><a href='{}'>{}</a></li>".format(f, f))
             
    # for root, dirs, files in os.walk(folder):
        # for file in files:
             #if file != 'index.html':
               #  if os.path.isdir(os.path.join(root, file)):
              #       index.write('<a href="{}">{}</a><br>\n'.format(os.path.join(root, file), file))
-    index.write('</body></html>\n')
+    index.write('{} {} </body></html>\n').format(Directories, Files)
     index.close()
 
 
