c6f512f8b6d109a2b29929aba5b61e3b5eeba7fc
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-11 22:11:15 -0500

    Update randoms.py

--- .github/workflows/randoms.py
+++ .github/workflows/randoms.py
@@ -14,6 +14,20 @@
 # use jinja template for easier customization. 
 # make function to make a single index in a single folder. (ie; do not add index.html to folder files.)
 
+## make some icons to be includes by converting png etc to base 64 and making basic style sheet like so 
+""" CSS example 
+
+span.swf
+{
+	background:
+	 url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAjZJREFUeNqMUk1rE1EUPfORptFEDNhgBaGQXQv+hC5m14Ibl0K3jVkUSgNuii0FF3WhCxclq4KF0G23pRi6KFhUZIKIQoWClkIpIbEYJ+l8ee7rmzELER+ceW/u3HPuvWee8R7A+soKDMO4z+M4/r2aURR9jcIQcRzjy8EBDBH4NjGBxtxcZWt1tX7Jj74gihAySRJlua6Lrb29R0okDI+SuPEBeH4NWLLkhQiJgPCJS8Jid9lKBa1WCw9nZvBgebnK8GsKHCmBT0B8g4csYWpin/CIHvGLuHV6imazmc7xstF4wu2pnO1gqKp0MDo/j9v1ukr87Di42N+HbZpweJa2C4UCdl23k4iZgW61r3Gd7XY3N+GS1CZZOhqxbSVCoxUpCEODkB32gIGMzCrVJyeRKZdxk7jj+/hYrSrx0UzmqlwQKJEwCIykA7uvWx/J5zG+sICw3cYbCgy0D6qAZaXzW1cCZirgaePura2hODuL440N/NSxxFCTpETE4h4MCwx0lbe1GkDE2tSIiLU/pp5dRCx6EQyPwIQXNHIp0qRoiCwoLy6mAukIvv+nAybXjkul2o7jPH63vf2MV1URI30LZZdYsgzl5dAIu2NjcKenJarqSDUhqV1fLrBtaBGJJyP0zs9hv+Lj7skJcsWirSowwdQd/E1EmF6vZ3udDr4fHqpvOR5KPzqdbNqmiJAgAmKawKaBNi+U/AXJFY5wpWqOKF2cnXXzU1Pr+I8VeV5XOGLHbwEGAJZPA5r+wu15AAAAAElFTkSuQmCC')
+	no-repeat
+	left center !important;
+	padding: 0px 0px 0px 25px;
+}
+
+""""
+
 ### Javascript To Do
 ## Click to sort. 
 ## Possibily a search feature bar.
