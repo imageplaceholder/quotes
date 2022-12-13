551c4ebb91b614596e5b0f49d7ca17e931cc2b65
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-12 20:05:42 -0500

    Update random2.py

--- .github/workflows/random2.py
+++ .github/workflows/random2.py
@@ -9,6 +9,8 @@ import shutil
 import glob
 import re
 
+FirstFolderProcessed = False
+
 def create_index(path, exclude_folders, exclude_files):
     """
                       self.index_file_content += '<tr><td valign="top"><img src="/icons/text.gif" alt="[TXT]"></td><td><a href="' + file + '">' + file + '</a></td><td align="right">' + str(file_last_modified) + '</td><td align="right">' + str(file_size) + '</td><td>&nbsp;</td></tr>\n'
@@ -19,6 +21,39 @@ def create_index(path, exclude_folders, exclude_files):
                     dir_last_modified = os.path.getmtime(dir_path)
                     self.index_file_content += '<tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="' + dir + '">' + dir + '</a></td><td align="right">' + str(dir_last_modified) + '</td><td align="right">' + str(dir_size) + '</td><td>&nbsp;</td></tr>\n'
     Create a function that creates a HTML index inside every folder and sub folder in a path. With option  to exclude folders by names and files by names.
+    
+    
+    
+    
+      self.index_file_content += '<html>\n'
+        self.index_file_content += '<head>\n'
+        self.index_file_content += '<title>Index of ' + self.path + '</title>\n'
+        self.index_file_content += '</head>\n'
+        self.index_file_content += '<body>\n'
+        self.index_file_content += '<h1>Index of ' + self.path + '</h1>\n'
+        self.index_file_content += '<table>\n'
+        self.index_file_content += '<tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>\n'
+        self.index_file_content += '<tr><th colspan="5"><hr></th></tr>\n'
+        self.index_file_content += '<tr><td valign="top"><img src="/icons/back.gif" alt="[PARENTDIR]"></td><td><a href="/">Parent Directory</a></td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>\n'
+        self.index_file_content += '<tr><th colspan="5"><hr></th></tr>\n'
+
+    def generate_index(self):
+        for root, dirs, files in os.walk(self.path):
+            for file in files:
+                if file not in self.exclude_files:
+                    file_path = os.path.join(root, file)
+                    file_size = os.path.getsize(file_path)
+                    file_last_modified = os.path.getmtime(file_path)
+                    self.index_file_content += '<tr><td valign="top"><img src="/icons/text.gif" alt="[TXT]"></td><td><a href="' + file + '">' + file + '</a></td><td align="right">' + str(file_last_modified) + '</td><td align="right">' + str(file_size) + '</td><td>&nbsp;</td></tr>\n'
+            for dir in dirs:
+                if dir not in self.exclude_folders:
+                    dir_path = os.path.join(root, dir)
+                    dir_size = os.path.getsize(dir_path)
+                    dir_last_modified = os.path.getmtime(dir_path)
+                    self.index_file_content += '<tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="' + dir + '">' + dir + '</a></td><td align="right">' + str(dir_last_modified) + '</td><td align="right">' + str(dir_size) + '</td><td>&nbsp;</td></tr>\n'
+        self.index_file_content += '</table>\n'
+        self.index_file_content += '</body>\n'
+        self.index_file_content += '</html>\n'
     """
     # Get all folders in path
     folders = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
@@ -37,22 +72,27 @@ def create_index(path, exclude_folders, exclude_files):
     index_file.write("<body>\n")
     index_file.write("<h1>Index of " + path + "</h1>\n")
     index_file.write("<hr>\n")
-    index_file.write("<pre>\n") 
-    index_file.write("<a href=\"../\">../</a>\n")
+    index_file.write("<pre>\n")
+    If FirstFolderProcessed is True:
+        index_file.write("<a href=\"../\">../</a>\n")
+    index_file.write("""<table>\n
+    '<tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>\n'
+    """)
     for folder in folders:
         dir_path = os.path.join(path, folder)
         print(dir_path)
         dir_size = os.path.getsize(dir_path)
         dir_last_modified = os.path.getmtime(dir_path)
-        folder_string = '<tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="' + folder + '">' + folder + '</a></td><td align="right">' + str(dir_last_modified) + '</td><td align="right">' + str(dir_size) + '</td><td>&nbsp;</td></tr>\n'
+        folder_string = '<tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="' + folder + '">' + folder + '</a></td><td align="right">' + str(dir_last_modified) + '</td><td align="right">' + str(dir_size) + '</td><td>&nbsp;</td></tr>\n' + '<tr><th colspan="5"><hr></th></tr>\n'
         index_file.write(folder_string)
     for file in files:
         file_path = os.path.join(path, file)
         print(file_path + "file")
         file_size = os.path.getsize(file_path)
         file_last_modified = os.path.getmtime(file_path)
-        file_string = '<tr><td valign="top"><img src="/icons/text.gif" alt="[TXT]"></td><td><a href="' + file + '">' + file + '</a></td><td align="right">' + str(file_last_modified) + '</td><td align="right">' + str(file_size) + '</td><td>&nbsp;</td></tr>\n'
+        file_string = '<tr><td valign="top"><img src="/icons/text.gif" alt="[TXT]"></td><td><a href="' + file + '">' + file + '</a></td><td align="right">' + str(file_last_modified) + '</td><td align="right">' + str(file_size) + '</td><td>&nbsp;</td></tr>\n' + '<tr><th colspan="5"><hr></th></tr>\n'
         index_file.write(file_string)
+    index_file.write('</table>\n')
     index_file.write("</pre>\n") 
     index_file.write("<hr>\n")
     index_file.write("</body>\n")
@@ -61,6 +101,7 @@ def create_index(path, exclude_folders, exclude_files):
     # Create index in sub folders
     for folder in folders:
         create_index(os.path.join(path, folder), exclude_folders, exclude_files)
+        FirstFolderProcessed = True
 
 def main():
     """
