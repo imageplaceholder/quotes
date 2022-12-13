1ee7457c24fb71fb6aa69cd46394efd4a6208466
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-12 21:26:51 -0500

    Update json_api.py

--- .github/workflows/json_api.py
+++ .github/workflows/json_api.py
@@ -1,56 +1,55 @@
 """
-Create a function that creates a JSON file with file paths of every folder and sub folder in a path with a JSON key in each file for pagination. With option  to exclude folders by names and files by names.
+Create a function that creates and saves a SVG animation of data from a user from Github API.
 """
 
-import os
-import json
-import argparse
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-def get_args():
-    parser = argparse.ArgumentParser(description='Create a JSON file with file paths of every folder and sub folder in a path with a JSON key in each file for pagination. With option  to exclude folders by names and files by names.')
-    parser.add_argument('-p', '--path', help='Path to the folder', required=True)
-    parser.add_argument('-e', '--exclude', help='Exclude folders by name', nargs='+')
-    parser.add_argument('-f', '--files', help='Exclude files by name', nargs='+')
-    parser.add_argument('-o', '--output', help='Output file name', required=True)
-    args = parser.parse_args()
-    return args
+# Make an API call and store the response.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
 
-def get_files(path, exclude, files):
-    files_list = []
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(".html"):
-                if exclude:
-                    if not any(x in root for x in exclude):
-                        if files:
-                            if not any(x in file for x in files):
-                                files_list.append(os.path.join(root, file))
-                        else:
-                            files_list.append(os.path.join(root, file))
-                else:
-                    if files:
-                        if not any(x in file for x in files):
-                            files_list.append(os.path.join(root, file))
-                    else:
-                        files_list.append(os.path.join(root, file))
-    return files_list
+# Store API response in a variable.
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
 
-def create_json(files_list, output):
-    json_data = {}
-    for file in files_list:
-        json_data[file] = {}
-        json_data[file]['pagination'] = {}
-        json_data[file]['pagination']['next'] = ''
-        json_data[file]['pagination']['previous'] = ''
-    with open(output, 'w') as outfile:
-        json.dump(json_data, outfile, indent=4)
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
 
-def main():
-   # args = get_args()
-    #files_list = get_files(args.path, args.exclude, args.files)
-   # files_lists = "./", [".github"], None
-    output = "./"
-    #create_json(files_list, output)
-    create_json(get_files("/", [".github"], None), "json_api")
-if __name__ == '__main__':
-    main()
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+
+    # Get the project description, if one is available.
+    description = repo_dict['description']
+    if not description:
+        description = "No description provided."
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': description,
+        'xlink': repo_dict['html_url'],
+        }
+    plot_dicts.append(plot_dict)
+
+# Make visualization.
+my_style = LS('#333366', base_style=LCS)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
