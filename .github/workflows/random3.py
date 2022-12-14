60c68aef6a991f32d9cb981462a759cb4c1fb8f1
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-14 00:08:42 -0500

    Update random3.py

--- .github/workflows/random3.py
+++ .github/workflows/random3.py
@@ -1,25 +1,59 @@
 """
-Create a function that writes a JSON file of every file, folder and sub folder in a path. Store each folder name in each object wrote. With option  to exclude folders by names and files by names.
+Write a JSON list of all contents inside every folder and sub folder in a path. Grouped by folder path. With option to exclude folders and files from list.
 """
 
 import os
 import json
+import argparse
 
-def write_json(path, exclude_folders=[], exclude_files=[]):
-    """
-    Write a JSON file of every file, folder and sub folder in a path. Store each folder name in each object wrote. With option  to exclude folders by names and files by names.
-    """
-    json_file = {}
-    for root, dirs, files in os.walk(path):
-        for folder in dirs:
-            if folder not in exclude_folders:
-                json_file[folder] = {"parent_dir": os.path.join(root, folder)}
-                for root_2, dirs_2, files_2 in os.walk(os.path.join(root, folder)):
-                    for file in files_2:
-                        if file not in exclude_files:
-                            json_file[folder][file] = {}
-    with open(os.path.join(path, 'json_file.json'), 'w') as f:
-        json.dump(json_file, f, indent=4)
+parser = argparse.ArgumentParser(description='Write a JSON list of all contents inside every folder and sub folder in a path. Grouped by folder path. With option to exclude folders and files from list.')
+parser.add_argument('path', help='Path to folder to list')
+parser.add_argument('-e', '--exclude', nargs='+', help='List of folders and files to exclude from list')
+parser.add_argument('-o', '--output', help='Output file name')
+args = parser.parse_args()
 
-if __name__ == '__main__':
-    write_json('./', exclude_folders=['.github/'], exclude_files=['file_1.txt', 'file_2.txt'])
+path = args.path
+exclude = args.exclude
+output = args.output
+
+if not output:
+    output = 'list.json'
+
+def list_files(path, exclude):
+    files = []
+    for file in os.listdir(path):
+        if file not in exclude:
+            files.append(file)
+    return files
+
+def list_folders(path, exclude):
+    folders = []
+    for folder in os.listdir(path):
+        if folder not in exclude:
+            folders.append(folder)
+    return folders
+
+def list_path(path, exclude):
+    files = list_files(path, exclude)
+    folders = list_folders(path, exclude)
+    return {
+        'files': files,
+        'folders': folders
+    }
+
+def list_path_recursive(path, exclude):
+    files = list_files(path, exclude)
+    folders = list_folders(path, exclude)
+    subfolders = {}
+    for folder in folders:
+        subfolders[folder] = list_path_recursive(path + '/' + folder, exclude)
+    return {
+        'files': files,
+        'folders': subfolders
+    }
+
+def write_json(path, data):
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+write_json("./", list_path_recursive("./", ["git", "github"]))
