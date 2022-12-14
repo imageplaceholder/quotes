ef47f3011876e6359f3cd8c29e51b2e9107ff356
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-13 22:30:45 -0500

    Update random3.py

--- .github/workflows/random3.py
+++ .github/workflows/random3.py
@@ -22,6 +22,7 @@ def get_repo_tree(repo_url):
     print(repo_tree)                    
     # loop through the tree
     for item in repo_tree:
+        item = repo_tree[int(item)]
         print(item)
         # if the item is a folder
         if item == 'tree':
