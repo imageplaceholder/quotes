f6457fec78b798f1f50fb3ced2c77b6e702e1d13
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-13 23:39:45 -0500

    Update random3.py

--- .github/workflows/random3.py
+++ .github/workflows/random3.py
@@ -1,78 +1,25 @@
-""" 
-Create a class to write a JSON file of all files and folders (including sub folders) download_urls from a GitHub repo with option to exclude file names and folders.
+"""
+Create a function that writes a JSON file of every file, folder and sub folder in a path. Store each folder name in each object wrote. With option  to exclude folders by names and files by names.
 """
 
 import os
 import json
-import requests
-import argparse
 
-class GitHubRepoDownloader:
-    def __init__(self, repo_url, exclude_file_names=None, exclude_folders=None):
-        self.repo_url = repo_url
-        self.exclude_file_names = exclude_file_names
-        self.exclude_folders = exclude_folders
-        self.repo_name = self.repo_url.split('/')[-1]
-        self.repo_owner = self.repo_url.split('/')[-2]
-        self.repo_contents_url = f'https://api.github.com/repos/{self.repo_owner}/{self.repo_name}/contents'
-        self.repo_contents = self.get_repo_contents()
-        self.repo_download_urls = self.get_repo_download_urls()
-        self.repo_download_urls_json = self.get_repo_download_urls_json()
-        self.write_repo_download_urls_json()
-
-    def get_repo_contents(self):
-        repo_contents = requests.get(self.repo_contents_url).json()
-        return repo_contents
-
-    def get_repo_download_urls(self):
-        repo_download_urls = []
-        for repo_content in self.repo_contents:
-            print(self.repo_contents[repo_content]['type'])
-            if repo_content[repo_content] == 'file':
-                if self.exclude_file_names:
-                    if repo_content['name'] not in self.exclude_file_names:
-                        repo_download_urls.append(repo_content['download_url'])
-                else:
-                    repo_download_urls.append(repo_content['download_url'])
-            elif repo_content[repo_content] == 'dir':
-                if self.exclude_folders:
-                    if repo_content['name'] not in self.exclude_folders:
-                        repo_download_urls.extend(self.get_repo_download_urls_from_folder(repo_content['url']))
-                else:
-                    repo_download_urls.extend(self.get_repo_download_urls_from_folder(repo_content['url']))
-        return repo_download_urls
-
-    def get_repo_download_urls_from_folder(self, folder_url):
-        folder_contents = requests.get(folder_url).json()
-        folder_download_urls = []
-        for folder_content in folder_contents:
-            if folder_content['type'] == 'file':
-                if self.exclude_file_names:
-                    if folder_content['name'] not in self.exclude_file_names:
-                        folder_download_urls.append(folder_content['download_url'])
-                else:
-                    folder_download_urls.append(folder_content['download_url'])
-            elif folder_content['type'] == 'dir':
-                if self.exclude_folders:
-                    if folder_content['name'] not in self.exclude_folders:
-                        folder_download_urls.extend(self.get_repo_download_urls_from_folder(folder_content['url']))
-                else:
-                    folder_download_urls.extend(self.get_repo_download_urls_from_folder(folder_content['url']))
-        return folder_download_urls
-
-    def get_repo_download_urls_json(self):
-        repo_download_urls_json = json.dumps(self.repo_download_urls)
-        return repo_download_urls_json
-
-    def write_repo_download_urls_json(self):
-        with open(f'{self.repo_name}_download_urls.json', 'w') as f:
-            f.write(self.repo_download_urls_json)
+def write_json(path, exclude_folders=[], exclude_files=[]):
+    """
+    Write a JSON file of every file, folder and sub folder in a path. Store each folder name in each object wrote. With option  to exclude folders by names and files by names.
+    """
+    json_file = {}
+    for root, dirs, files in os.walk(path):
+        for folder in dirs:
+            if folder not in exclude_folders:
+                json_file[folder] = {}
+                for root_2, dirs_2, files_2 in os.walk(os.path.join(root, folder)):
+                    for file in files_2:
+                        if file not in exclude_files:
+                            json_file[folder][file] = {}
+    with open(os.path.join(path, 'json_file.json'), 'w') as f:
+        json.dump(json_file, f, indent=4)
 
 if __name__ == '__main__':
-    GitHubRepoDownloader("https://github.com/imageplaceholder/quotes", [], [])
-   # parser = argparse.ArgumentParser()
-   # parser.add_argument('repo_url', help='GitHub repo url')
-   # parser.add_argument('--exclude_file_names', nargs='+', help='List of file names to exclude')
-   # parser.add_argument('--exclude_folders', nargs='+', help='List of folders to exclude')
-   # args = parser.parse_args()
-    #GitHubRepoDownloader(args.repo_url, args.exclude_file_names, args.exclude_folders)
+    write_json('./', exclude_folders=['.github/'], exclude_files=['file_1.txt', 'file_2.txt'])
