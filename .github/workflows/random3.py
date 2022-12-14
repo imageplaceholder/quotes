b537873c8f2d33acb5b6442f6b96b9e1ab4373c5
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-14 04:57:13 -0500

    Create random3.py

--- .github/workflows/random3.py
+++ .github/workflows/random3.py
@@ -1,61 +1,14 @@
-"""
-Write a JSON list of all contents inside every folder and sub folder in a path. Grouped by folder path. With option to exclude folders and files from list.
-"""
-
 import os
 import json
-import argparse
 
-#parser = argparse.ArgumentParser(description='Write a JSON list of all contents inside every folder and sub folder in a path. Grouped by folder path. With option to exclude folders and files from list.')
-#parser.add_argument('path', help='Path to folder to list')
-#parser.add_argument('-e', '--exclude', nargs='+', help='List of folders and files to exclude from list')
-#parser.add_argument('-o', '--output', help='Output file name')
-#args = parser.parse_args()
+def path_to_dict(path):
+    d = {'name': os.path.basename(path)}
+    if os.path.isdir(path):
+        d['type'] = "directory"
+        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
+(path)]
+    else:
+        d['type'] = "file"
+    return d
 
-#path = args.path
-#exclude = args.exclude
-output = ""
-
-if not output:
-    output = 'list.json'
-
-def list_files(path, exclude):
-    files = []
-    for file in path:
-        if os.path.isdir(file):
-            if file not in exclude:
-                files.append(file)
-    return files
-
-def list_folders(path, exclude):
-    folders = []
-    for folder in path:
-        if os.path.isdir(folder):
-            if folder not in exclude:
-                folders.append(folder)        
-    return folders
-
-def list_path(path, exclude):
-    files = list_files(path, exclude)
-    folders = list_folders(path, exclude)
-    return {
-        'files': files,
-        'folders': folders
-    }
-
-def list_path_recursive(path, exclude):
-   # files = list_files(path, exclude)
-    folders = list_folders(path, exclude)
-    subfolders = {}
-    for folder in folders:
-        subfolders[folder] = list_path_recursive(path + '/' + folder, exclude)
-    return {
-        'files': files,
-        'folders': subfolders
-    }
-
-def write_json(path, data):
-    with open(path, 'w') as f:
-        json.dump(data, f, indent=4)
-
-write_json(".", list_path_recursive(".", [".git", ".github", ".git/", ".github", "git" "github"]))
+print json.dumps(path_to_dict('.'))
