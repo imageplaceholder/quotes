86ce0ecc5b4819f9f372daa1f0b4afd1d5b39006
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-12-15 04:47:06 -0500

    Update random3.py

--- .github/workflows/random3.py
+++ .github/workflows/random3.py
@@ -4,11 +4,12 @@ import json
 def path_to_dict(path):
     d = {'name': os.path.basename(path)}
     if os.path.isdir(path):
-        d['type'] = "directory"
-        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
+        d['isDirectory'] = "true"
+        d['content'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
 (path)]
     else:
-        d['type'] = "file"
+        d['isDirectory'] = "false"
+        d['size'] = 0
     return d
 
 data = path_to_dict('.')
